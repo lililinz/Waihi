04a63ae1465fd77717c3cce69e3d270f4c3e8fc7
lililinz <lili.li@outlook.co.nz>
2024-06-25 23:30:22 +1200

    Add files via upload

---
+++ styles/nzaddresses_5_style.js
@@ -0,0 +1,30 @@
+var size = 0;
+var placement = 'point';
+
+var style_nzaddresses_5 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    ''
+    var labelText = "";
+    size = 0;
+    var labelFont = "13.0px \'Arial\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "#fafafa";
+    var bufferWidth = 1.0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if (feature.get("full_addre") !== null) {
+        labelText = String(feature.get("full_addre"));
+    }
+    
+    var style = [ new ol.style.Style({
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor, bufferWidth)
+    })];;
+
+    return style;
+};
